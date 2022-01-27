a67eddd35283ed3c5f9c0b84e4831d46eed6e143
Mikayla Dobson <mikaylaherself@gmail.com>
2022-01-27 11:25:51 -0600

    more light styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './features/navbar/Navbar';
+import Post from './features/posts/Post';
 import redditSlice from './features/reddit/redditSlice';
 
 function App() {
@@ -8,6 +9,26 @@ function App() {
     <div className="App">
       <Navbar />
       <p>Stuff</p>
+      <div className="content-container">
+
+        <div className="feed">
+          {/* To do: import posts from post directory */}
+          {/* Map post data onto individual post cards, handle undefined values */}
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+        
+        <div className="about-the-site">
+          {/* To do: add mutable state to class name for this div, */}
+          {/* determining whether or not it's active based on the state of */}
+          {/* The action dispatched from the searchbar slice(?) */}
+          {/* Do I need a searchbar slice? */}
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/features/posts/Post.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Post.css';
+
+export default function Post() {
+    return (
+        <>
+        <div className="post-body">
+            <h1 className="title">Post title</h1>
+            <div className="image-placeholder"></div>
+            <div className="post-metadata">
+                <p className="user">u/username</p>
+                <p className="time-posted">posted at midnight</p>
+                <p className="num-comments">a million comments</p>
+            </div>
+        </div>
+        </>
+    );
+}
