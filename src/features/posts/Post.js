4577d8fbd4a909d03fef7ee723520c35df1bd43a
Mikayla Dobson <mikaylaherself@gmail.com>
2022-01-27 15:52:42 -0600

    preparing for experimental changes to feed

--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -6,7 +6,9 @@ export default function Post({title,author,subreddit,ups,comments,time,key,media
     const [body, setBody] = useState(selftext);
     
     useEffect(() => {
-        if (selftext.length > limit) {
+        if (selftext.length === 0) {            // in the case that the post body is empty, it does not include an ellipsis on mouseout
+            return;
+        } else if (selftext.length > limit) {
             setBody(selftext.substring(0,limit) + '...');
         } else {
             return;
@@ -18,6 +20,10 @@ export default function Post({title,author,subreddit,ups,comments,time,key,media
     }
 
     const handleMouseOut = () => {
+        if (selftext.length === 0) {        // ...and then doesn't add it in after a mouseover/out
+            return;
+        }
+
         setBody(selftext.substring(0,limit) + '...');
     }
     
@@ -25,13 +31,13 @@ export default function Post({title,author,subreddit,ups,comments,time,key,media
         <>
         <div className="post-body" onMouseOver={handleHover} onMouseOut={handleMouseOut}>
             <a className="title" href={`https://reddit.com${permalink}`}>{title ? title : 'title'}</a>
-            {media ? <img alt={title} src={media} /> : ''}
-            {video ? <video controls type="video/mp4" src={video}></video> : ''}
+            {media ?     <img alt={title} src={media} />                           : ''}
+            {video ?     <video controls type="video/mp4" src={video}></video>     : ''}
             <p className="post-text">{body}</p>
             <div className="post-metadata">
                 <p className="user">{author ? 'u/' + author : 'u/username'}</p>
-                <p className="time-posted">{time ? time : ''}</p>
-                <p className="num-comments">{comments ? comments : 'comments'}</p>
+                <p className="time-posted">posted at {time ? time : '...?'}</p>
+                <p className="num-comments">{comments ? comments : 'no'} comments</p>
             </div>
         </div>
         </>
